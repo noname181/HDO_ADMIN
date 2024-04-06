c9f8947c3444ae40a745be439262c4a4174e57aa
Noname <l.h.thuong181@gmail.com>
2024-04-07 05:05:07 +0700

    Update AUIGridLicense.js

--- src/AUI/AUIGrid/AUIGridLicense.js
+++ src/AUI/AUIGrid/AUIGridLicense.js
@@ -1,10 +1,10 @@
 /*!
- * AUIGrid 3.0.13 License
+ * AUIGrid 3.0.14 License
  * License Type : Evaluation(Trial) Version
- * Authorized Domain(or IP) : localhost,127.0.0.1
- * Expiration Date : 2024-04-27
+ * Authorized Domain(or IP) : localhost,127.0.0.1,158.247.249.66
+ * Expiration Date : 2024-05-07
  * www.auisoft.net
  */
  /* eslint-disable */
-var AUIGridLicense = "eyJjdCI6InNBeU45aWdYaGNsV1FxWnhTOFUxRkRBVGVRZG95NitqaG0rUWNpak9IdVZqZWxrdkl2SE95RFhsaFNvTEVNNzhCaDROdFhxZlFJQUcrc1IyUldQZ3pMNDdWTGtCeEljTHV1MGJ5NjJpZjIwPSIsIml2IjoiYmUzMmJjNjJlNTg1MTY4MzM2YmIyYmQ2ZjNmMGZkYWQiLCJzIjoiODM0NDBiYmI0YzQ5ZmZmZCJ9";
+var AUIGridLicense = "eyJjdCI6IkNyMmc3VEloaVN4WXJoeXpjbHIwTmNVcmduXC9GN1wvYkxBYTdXdGhYQ2VHSFlmOWJEZ2hJWk9hK01NZGYrV0c1d09TZHRXXC9XUXN0a0FVbjlIbEJOZWc0bXZkVHg3bXRSQk04ZENXWnE4YXMwPSIsIml2IjoiN2Y0NzFmZTQzZWMzNmJkOGM5OWMwYWYzY2ZmN2I5MmIiLCJzIjoiODExMTUwODM4MThiMjNlZCJ9";
 if (typeof window !== "undefined") window.AUIGridLicense = AUIGridLicense;
